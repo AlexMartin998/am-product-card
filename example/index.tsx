c5f4eb6b9a75eda9fde071a776a5cc5e86d30411
Alex Martin <alextravc.342@gmail.com>
2022-05-11 09:44:21 -0500

    Version 1.0.0-rc completed

--- example/index.tsx
+++ example/index.tsx
@@ -9,7 +9,7 @@ const App = () => {
       <ProductCard
         product={{
           id: '1',
-          title: 'Coffee mug - card',
+          title: 'Coffee mug - card!',
           // img: './coffee-mug.png',
         }}
         initialValues={{
